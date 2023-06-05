7260a227cd07128c87bd9f9527b91392e7892948
OveralZ <wangr7636@gmail.com>
2023-06-06 11:44:44 +1200

    Removed unnecessary flowchart file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from win32 import win32gui
-from win32 import win32process
-from win32.lib import win32con
-from tkinter import *
+#Libraries Setup
+from pywinauto import Application
+import win32process, win32gui, win32con, time
 
+from tkinter import *
+from tkinter import ttk
